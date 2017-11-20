94b3db1380612e05c7c5c19aeea1d2fe1cc6b4d0
Duncan McAlester <duncan@rocketcom.com>
2017-11-20 15:27:55 -0800

    gulp will now launch polymer before initing BrowserSync

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp        = require('gulp');
-var exec = require('child_process').exec;
+// var exec = require('child_process').exec;
+var spawn = require('child_process').spawn;
 var browserSync = require('browser-sync').create();
 
 
@@ -19,14 +20,36 @@ var browserSync = require('browser-sync').create();
 // 	}
 // }
 
+// cb = function(err) {
+// 	console.log('asdf');
+// 	browserSync.init({
+// 		// server: "./index.html"
+// 		proxy: "http://127.0.0.1:8081"
+
+// 	});
+// 	gulp.watch("public/css/**/*.css", ['styles']);
+// 	gulp.watch("public/*.html").on('change', browserSync.reload);
+// 	gulp.watch("index*.html").on('change', browserSync.reload);
+// }
+
 // Static Server + watching scss/html files
 gulp.task('serve', ['styles'], function(cb) {
-	exec('polymer serve', function (err, stdout, stderr) {
-		console.log(stdout);
-		console.log(stderr);
-		cb(err);
-	});
-});
+	var process = spawn('polymer serve', [], { shell: true });
+			process.stdout.on('data', (data) => {
+				console.log('data',data);
+				if(data) {
+					browserSync.init({
+						proxy: "http://127.0.0.1:8081"
+					});
+
+					gulp.watch("public/css/**/*.css", ['styles']);
+					gulp.watch("public/*.html").on('change', browserSync.reload);
+					gulp.watch("index*.html").on('change', browserSync.reload);
+				}
+			})
+	
+})
+
 
 // Compile sass into CSS & auto-inject into browsers
 gulp.task('styles', function() {
