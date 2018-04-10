a92a589dd4cbd4ff7e3cf45de413fcb0a09343ef
Duncan McAlester <duncan@rocketcom.com>
2018-04-10 10:13:24 -0700

    Added template to rux-component for demos

---
+++ src/astro-components/rux-component/demo/rux-component-sample.js
@@ -0,0 +1,37 @@
+import {
+  html,
+  Element as PolymerElement
+} from "/node_modules/@polymer/polymer/polymer-element.js";
+import { RuxComponent } from "/src/astro-components/rux-component/rux-component.js";
+/**
+ * @polymer
+ * @extends HTMLElement
+ */
+export class RuxComponentSample extends PolymerElement {
+  static get template() {
+    return html`<rux-component></rux-component>`;
+  }
+  static get properties() {
+    return {
+      name: {
+        type: String,
+        value: "Template Component"
+      }
+    };
+  }
+
+  constructor() {
+    super();
+  }
+
+  connectedCallback() {
+    super.connectedCallback();
+  }
+  disconnectedCallback() {
+    super.disconnectedCallback();
+  }
+  ready() {
+    super.ready();
+  }
+}
+customElements.define("rux-component-sample", RuxComponentSample);
