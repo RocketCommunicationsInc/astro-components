a9bed5312bd119afac2448561d38fa41c6aff261
Duncan McAlester <duncan@rocketcom.com>
2018-08-15 14:26:31 -0700

    Added astro 3.0 styles to toggle buttons including light/dark. Missing 'nub' svg image

--- src/astro-components/rux-toggle/rux-toggle.js
+++ src/astro-components/rux-toggle/rux-toggle.js
@@ -29,117 +29,144 @@ export class RuxToggle extends PolymerElement {
     return html`
       <style>
       :host {
-        box-sizing: border-box;
-      
-        margin: 0 2px;
-        font-weight: 600;
-        font-size: 12px;
-      
-        outline: 0;
-        display: inline-block;
-      
-        border: 1px solid #101923;
-        height: 1.375rem;
-        width: 4.375rem;
-      
         position: relative;
-      
-        background: linear-gradient(
-          to right,
-          #005a8f 0%,
-          #005a8f 50%,
-          #000 50%,
-          #000 100%
-        );
-        box-shadow: inset 0 0 4px rgba(0, 0, 0, 0.5);
+        display: inline-block;
+        box-sizing: border-box;
+
+        -webkit-font-smoothing: subpixel-antialiased;
+
+        height: 1.3125rem;
+        width: 4.375rem;
+        border-radius: var(--defaultBorderRadius, 0.1875rem);
+        border: 1px solid var(--toggleBaseBorderColor, rgb(0, 0, 0));
+        // box-shadow: inset 0 0 2px rgba(0, 0, 0, 1);
+        user-select: none;
+        overflow: hidden;
+        
       
         -webkit-user-select: none;
         -moz-user-select: none;
         -ms-user-select: none;
         user-select: none;
+        
       }
       
-      *,
-      *::after *::before {
-        box-sizing: inherit;
-      }
-      
-      :host::before,
-      :host::after {
-        display: inline-block;
-      
-        top: 0;
-        position: absolute;
-        color: #fff;
-        height: 100%;
-        width: 50%;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        padding-top: 1px;
-      }
-      
-      :host::before {
-        content: "ON";
-        left: 0;
-      }
-      
-      :host::after {
-        content: "OFF";
-        right: 0;
+      :host([disabled]) {
+        opacity: var(--disabledOpacity, 0.4);
       }
       
       :host input {
         display: none !important;
       }
-      
-      /* .rux-toggle__input {
-        display: none;
-      } */
-      
+
       .rux-toggle__button {
-        display: block;
         position: absolute;
+        display: flex;
+
+        justify-content: space-around;
+        align-content: center;
+        align-items: center;
+
         top: 0;
         left: 0;
+
+        font-size: 0.75rem !important;
+
         height: 100%;
-      
         width: 100%;
-      
-        cursor: pointer;
-        z-index: 20;
       }
-      .rux-toggle__button::after {
-        box-sizing: border-box;
-        position: relative;
-        display: block;
-        content: "";
+
+      .rux-toggle__button span {
+        display: flex;
+      
+        align-content: center;
+        justify-content: center;
+        align-items: center;
+      
         width: 50%;
         height: 100%;
-        background-color: #005a8f;
-        border-top: 1px solid rgba(255, 255, 255, 0.1);
-        border-bottom: 1px solid rgba(0, 0, 0, 0.15);
-        transition: left 0.1s ease-out;
+        z-index: 1;
+      
+        text-transform: uppercase;
+      }
+
+      .rux-toggle__button::before {
+        position: absolute;
+       
+        content: "";
+        display: block;
+      
+        height: 100%;
+        width: 100%;
+
+        background: var(
+          --toggleBaseBackgroundColor,
+          linear-gradient(
+            to right,
+            #000 50%,
+            rgb(50, 51, 52) 50.1%,
+            rgb(50, 51, 52) 100%
+          )
+        );
+      }
+
+      .rux-toggle__button::after {
+        content: "";
+
+        display: block;
+
+        position: absolute;
+
+        box-sizing: border-box;
+
         top: 0;
         left: 0;
+
         z-index: 10;
-        box-shadow: 0 0 4px rgba(0, 0, 0, 0.5);
+
+        transition: left 0.1s ease-out;
+    
+        width: 50%;
+        height: 100%;
+      
+        border-radius: 0.125rem;
+          
+        background-color: var(--toggleButtonBackgroundColor, rgb(0, 90, 143));
+
+        box-shadow: var(
+          --toggleButtonBoxShadow,
+          0 0 3px rgba(0, 0, 0, 0.5),
+          1px 0 2px rgba(0, 0, 0, 0.6)
+        );
+      }
+
+      .on {
+        color: var(--toggleBaseSelectedTextColor, rgb(91, 255, 0));
       }
       
+      .off {
+        color: var(--toggleBaseTextColor, rgb(198, 204, 209));
+      }
+          
       .rux-toggle__input:checked + .rux-toggle__button::after {
         left: 50%;
+        box-shadow: var(
+          --toggleButtonSelectedBoxShadow,
+          0 0 3px rgba(0, 0, 0, 0.5),
+          -1px 0 2px rgba(0, 0, 0, 0.6)
+        );
       }
       
-      :host([disabled]) {
-        opacity: 0.4;
-      }
       .rux-toggle__input:disabled + .rux-toggle__button {
         cursor: not-allowed;
       }
-</style>      
+      </style>      
       
       <input class="rux-toggle__input" type="checkbox" id="[[_id]]" disabled$=[[disabled]] checked={{checked::change}}></input>
-      <label for$="[[_id]]" class="rux-toggle__button">[[_label]]</label> 
+      <label for$="[[_id]]" class="rux-toggle__button">
+        <span class='on'>On</span>
+        <span class='off'>Off</span>
+      </label> 
       `;
   }
 
