f58dc261fb7a1e018bf7a97fd9ebb2b52be76aa3
Duncan McAlester <duncan@rocketcom.com>
2018-09-18 14:21:25 -0700

    Updated toggle demo

--- packages/rux-toggle/demo/rux-toggle-sample.js
+++ packages/rux-toggle/demo/rux-toggle-sample.js
@@ -7,21 +7,38 @@ import { RuxToggle } from "../rux-toggle.js";
 export class RuxToggleSample extends PolymerElement {
   static get template() {
     return html`
+
+    <style>
+
+      .toggle-buttons {  
+        padding: 0;
+        max-width: 200px;
+      }
+      .toggle-buttons .rux-form-field {
+        width: 100%;
+        display: flex;
+        justify-content: space-between;
+
+        margin-bottom: 1rem;
+      }
+
+
+    </style>
 		<ul class="toggle-buttons">
-				<li>
+				<li class="rux-form-field">
 					<div id="toggle1">Toggle 1:</div>
 					<rux-toggle aria-labeledby="toggle1"></rux-toggle>
 				</li>
-				<li>
+				<li class="rux-form-field">
 					<div id="toggle2">Toggle 2:</div>
 					<rux-toggle checked aria-labeledby="toggle1">
 						<rux-toggle>
 				</li>
-				<li>
+				<li class="rux-form-field">
 					<div id="toggle3">Toggle 3:</div>
 					<rux-toggle disabled aria-labeledby="toggle1"></rux-toggle>
 				</li>
-				<li>
+				<li class="rux-form-field">
 					<div id="toggle4">Toggle 4:</div>
 					<rux-toggle checked disabled aria-labeledby="toggle1"></rux-toggle>
 				</li>
