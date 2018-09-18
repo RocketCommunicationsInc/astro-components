dce2ff44321737abe1c557b24f41a1f3b932eeac
Duncan McAlester <duncan@rocketcom.com>
2018-09-18 14:01:55 -0700

    Updated status demo

--- packages/rux-status/demo/rux-status-sample.js
+++ packages/rux-status/demo/rux-status-sample.js
@@ -1,4 +1,5 @@
 import { PolymerElement, html } from "@polymer/polymer/polymer-element.js";
+import { RuxIcon } from "../../rux-icon/rux-icon.js";
 import { RuxStatus } from "../rux-status.js";
 /**
  * @polymer
@@ -6,7 +7,15 @@ import { RuxStatus } from "../rux-status.js";
  */
 export class RuxStatusSample extends PolymerElement {
   static get template() {
-    return html`<rux-status status="ok"></rux-status>`;
+    return html`
+    <h1>test</h1>
+    <rux-status status="off"></rux-status>
+    <rux-status status="standby"></rux-status>
+    <rux-status status="normal"></rux-status>
+    <rux-status status="caution"></rux-status>
+    <rux-status status="serious"></rux-status>
+    <rux-status status="critical"></rux-status>
+    `;
   }
   static get properties() {
     return {
