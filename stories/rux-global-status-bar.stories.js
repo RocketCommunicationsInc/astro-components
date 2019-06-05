6178317057adb43bc59be929c03bf3bed04bc5a1
Duncan McAlester <duncan@rocketcom.com>
2019-06-05 14:35:30 -0700

    Minor Fix for Global Status Bar Story Missing RuxButton import was causing an issue

--- stories/rux-global-status-bar.stories.js
+++ stories/rux-global-status-bar.stories.js
@@ -4,88 +4,81 @@ import { html, render } from 'lit-html';
 import { text, boolean, date, withKnobs } from '@storybook/addon-knobs';
 import { RuxGlobalStatusBar } from '../src/components/rux-global-status-bar/rux-global-status-bar.js';
 import { RuxClock } from '../src/components/rux-clock/rux-clock.js';
+import { RuxButton } from '../src/components/rux-button/rux-button.js';
 import { RuxTabs } from '../src/components/rux-tabs/rux-tabs.js';
 import Readme from '../src/components/rux-global-status-bar/README.md';
 
 /* eslint-enable no-unused-vars */
 
 storiesOf('Components|Global Status Bar', module)
-  .addDecorator(withKnobs)
-  .add(
-    'Global Status Bar',
-    () => { 
+    .addDecorator(withKnobs)
+    .add(
+        'Global Status Bar',
+        () => {
+          const appnameKnob = text('App Name', 'Astro Global Status Bar');
+          const versionKnob = text('Version', '4.1.0 alpha');
 
-      const appnameKnob = text('App Name', 'Astro Global Status Bar');
-      const versionKnob = text('Version', '4.1.0 alpha');
-
-
-      return html`
+          return html`
         <div style="display: flex; justify-content: center;">
-          <rux-global-status-bar
-            .appname="${appnameKnob}" 
-            .version="${versionKnob}"
-            ></rux-global-status-bar>
+          <rux-global-status-bar .appname="${appnameKnob}" .version="${versionKnob}"></rux-global-status-bar>
         </div>
-      `
-    },{
-      exports: {
-        render,
-        html,
-      },
-      notes: {
-        markdown: Readme,
-      },
-    },  
-  )
-  .add(
-    'Global Status Bar with Slot Content',
-    () => { 
+      `;
+        },
+        {
+          exports: {
+            render,
+            html,
+          },
+          notes: {
+            markdown: Readme,
+          },
+        }
+    )
+    .add(
+        'Global Status Bar with Slot Content',
+        () => {
+          function dateWrapper(name, defaultValue) {
+            const stringTimestamp = date(name, defaultValue);
+            return new Date(stringTimestamp);
+          }
 
-      function dateWrapper(name, defaultValue) {
-        const stringTimestamp = date(name, defaultValue)
-        return new Date(stringTimestamp)
-      }
+          const appnameKnob = text('App Name', 'Astro Global Status Bar');
+          const versionKnob = text('Version', '4.1.0 alpha');
 
-      const appnameKnob = text('App Name', 'Astro Global Status Bar');
-      const versionKnob = text('Version', '4.1.0 alpha');
-
-      return html`
+          return html`
         <div style="display: flex; justify-content: center;">
-          <rux-global-status-bar class="dark-theme"
-            .appname="${appnameKnob}" 
-            .version="${versionKnob}">
+          <rux-global-status-bar class="dark-theme" .appname="${appnameKnob}" .version="${versionKnob}">
             <rux-clock></rux-clock>
             <div><!--Any HTML here --></div>
             <rux-button>Master Off</rux-button>
           </rux-global-status-bar>
         </div>
-      `
-    },{
-      exports: {
-        render,
-        html,
-      },
-      notes: {
-        markdown: Readme,
-      },
-    },  
-  ).add(
-    'Global Status Bar with Tabs',
-    () => { 
+      `;
+        },
+        {
+          exports: {
+            render,
+            html,
+          },
+          notes: {
+            markdown: Readme,
+          },
+        }
+    )
+    .add(
+        'Global Status Bar with Tabs',
+        () => {
+          function dateWrapper(name, defaultValue) {
+            const stringTimestamp = date(name, defaultValue);
+            return new Date(stringTimestamp);
+          }
 
-      function dateWrapper(name, defaultValue) {
-        const stringTimestamp = date(name, defaultValue)
-        return new Date(stringTimestamp)
-      }
+          const appnameKnob = text('App Name', 'Astro Global Status Bar');
+          const versionKnob = text('Version', '4.1.0 alpha');
 
-      const appnameKnob = text('App Name', 'Astro Global Status Bar');
-      const versionKnob = text('Version', '4.1.0 alpha');
-
-      return html`
+          return html`
         <div style="display: flex; flex-flow: column; justify-content: center;">
-          <rux-global-status-bar class="dark-theme"
-            .appname="${appnameKnob}" 
-            .version="${versionKnob}">
+          <rux-global-status-bar class="dark-theme" .appname="${appnameKnob}" .version="${versionKnob}">
             <rux-tabs id="tab-set-id-1">
               <rux-tab id="tab-id-1-1">Tab 1</rux-tab>
               <rux-tab id="tab-id-1-2">Tab 2</rux-tab>
@@ -95,24 +88,31 @@ storiesOf('Components|Global Status Bar', module)
           </rux-global-status-bar>
           <rux-tab-panels aria-labelledby="tab-set-id-1">
             <rux-tab-panel aria-labelledby="tab-id-1-1">
-            <pre style="padding: 1vw; border: rgba(255,255,255, .15) dashed 1px; margin: 0;"><<span>!-- Tab 1 HTML Content --</span>></pre>
+              <pre
+                style="padding: 1vw; border: rgba(255,255,255, .15) dashed 1px; margin: 0;"
+              ><<span>!-- Tab 1 HTML Content --</span>></pre>
             </rux-tab-panel>
             <rux-tab-panel aria-labelledby="tab-id-1-2">
-            <pre style="padding: 1vw; border: rgba(255,255,255, .15) dashed 1px; margin: 0;"><<span>!-- Tab 2 HTML Content --</span>></pre>
+              <pre
+                style="padding: 1vw; border: rgba(255,255,255, .15) dashed 1px; margin: 0;"
+              ><<span>!-- Tab 2 HTML Content --</span>></pre>
             </rux-tab-panel>
             <rux-tab-panel aria-labelledby="tab-id-1-3">
-            <pre style="padding: 1vw; border: rgba(255,255,255, .15) dashed 1px; margin: 0;"><<span>!-- Tab 3 HTML Content --</span>></pre>
+              <pre
+                style="padding: 1vw; border: rgba(255,255,255, .15) dashed 1px; margin: 0;"
+              ><<span>!-- Tab 3 HTML Content --</span>></pre>
             </rux-tab-panel>
           </rux-tab-panels>
         </div>
-      `
-    },{
-      exports: {
-        render,
-        html,
-      },
-      notes: {
-        markdown: Readme,
-      },
-    },  
-  )
+      `;
+        },
+        {
+          exports: {
+            render,
+            html,
+          },
+          notes: {
+            markdown: Readme,
+          },
+        }
+    );
