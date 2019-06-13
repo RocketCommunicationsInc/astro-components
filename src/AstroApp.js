3cac3e5e585f02b52d64f909a25271caaf05d754
Duncan McAlester <duncan@rocketcom.com>
2019-06-13 14:47:35 -0700

    Adding a scratchpad for more complicated component development

--- src/AstroApp.js
+++ src/AstroApp.js
@@ -1,31 +1,108 @@
-import { LitElement, html } from 'lit-element';
+import { html, css, LitElement } from 'lit-element';
 /* eslint-disable no-unused-vars */
 import { RuxClock } from './components/rux-clock/rux-clock';
+import { RuxPopUpMenu } from './components/rux-pop-up-menu/rux-pop-up-menu';
 /* eslint-enable no-unused-vars */
 
-/** Class representing a single Clock instance. */
-export class AstroApp extends LitElement {
+export default class AstroApp extends LitElement {
+  static get styles() {
+    return css`
+      #demo {
+        padding: 5%;
+        display: flex;
+        justify-content: center;
+      }
+
+      #pop-demo {
+        position: absolute;
+        top: 0;
+        left: 0;
+        height: 100vh;
+        width: 100vw;
+        display: flex;
+      }
+
+      button {
+        display: inline-block;
+        position: absolute;
+      }
+
+      #tl {
+        top: 0;
+        left: 0;
+      }
+
+      #tr {
+        top: 0;
+        right: 0;
+      }
+
+      #bl {
+        bottom: 0;
+        left: 0;
+      }
+
+      #br {
+        bottom: 0;
+        right: 0;
+      }
+
+      #tc {
+        top: 0;
+        right: 50vw;
+      }
+
+      #bc {
+        bottom: 0;
+        right: 50vw;
+      }
+    `;
+  }
+
   static get properties() {
     return {
-      appName: {
-        type: String,
-      },
-      _progress: {
-        type: Number,
+      heading: { type: String },
+      data: {
+        type: Array,
       },
     };
   }
 
   constructor() {
     super();
-    this.appName = 'Astro 4.0';
+    this.heading = 'Hello world!';
+    this.data = [
+      {
+        id: '1',
+        label: 'Item 1',
+      },
+      {
+        id: '2',
+        label: 'Item 2',
+      },
+      {
+        id: '3',
+        label: 'Item 4',
+        role: 'seperator',
+      },
+    ];
   }
 
   render() {
     return html`
-      <h1>Astro 4.0</h1>
-      <rux-clock></rux-clock>
+      <div id="demo">
+        <rux-clock></rux-clock>
+        <div id="pop-demo">
+          <button id="tl">tl</button>
+          <button id="tr">tr</button>
+          <button id="bl">bl</button>
+          <button id="br">br</button>
+          <button id="tc">tv</button>
+          <button id="bc">bc</button>
+        </div>
+
+        <rux-pop-up-menu .data="${this.data}"></rux-pop-up-menu>
+      </div>
     `;
   }
 }
-customElements.define('astro-app', AstroApp);
