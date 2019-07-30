b3f5ecaa977b879d32feaa9c206fbdb40b624264
Duncan McAlester <duncan@rocketcom.com>
2019-07-30 14:48:22 -0700

    Updated to Storybook 5.1

--- stories/rux-tree.stories.js
+++ stories/rux-tree.stories.js
@@ -1,101 +1,86 @@
 /* eslint-disable no-unused-vars */
-import { storiesOf, html } from '@open-wc/demoing-storybook';
-import { withKnobs } from '@storybook/addon-knobs';
+import { storiesOf } from '@storybook/polymer';
+import { html, render } from 'lit-html';
 import { RuxTree } from '../src/components/rux-tree/rux-tree.js';
 import { RuxStatus } from '../src/components/rux-status/rux-status.js';
 import Readme from '../src/components/rux-tree/README.md';
+import { withKnobs } from '@storybook/addon-knobs';
 /* eslint-enable no-unused-vars */
 
 storiesOf('Components|Tree', module)
     .addDecorator(withKnobs)
-    .add(
-        'Tree',
-        () => {
-          const treeData = [
+    .add('Tree', () => {
+      const treeData = [
+        {
+          label: 'Tree Item 1',
+          expanded: true,
+          selected: true,
+          children: [
             {
-              label: 'Tree Item 1',
-              expanded: true,
-              selected: true,
+              label: 'Tree Item 1.1',
+              children: [{ label: 'Tree Item 1.1.1' }, { label: 'Tree Item 1.1.2' }, { label: 'Tree Item 1.1.3' }],
+            },
+            { label: 'Tree Item 1.2' },
+            {
+              label: 'Tree Item 1.3',
+              expanded: false,
               children: [
                 {
-                  label: 'Tree Item 1.1',
+                  label: 'Tree Item 1.3.1',
                   children: [{ label: 'Tree Item 1.1.1' }, { label: 'Tree Item 1.1.2' }, { label: 'Tree Item 1.1.3' }],
                 },
-                { label: 'Tree Item 1.2' },
                 {
-                  label: 'Tree Item 1.3',
-                  expanded: false,
+                  label: 'Tree Item 1.3.2',
+                  children: [{ label: 'Tree Item 1.1.1' }, { label: 'Tree Item 1.1.2' }, { label: 'Tree Item 1.1.3' }],
+                },
+                {
+                  label: 'Tree Item 1.3.3',
+                  expanded: true,
                   children: [
-                    {
-                      label: 'Tree Item 1.3.1',
-                      children: [{ label: 'Tree Item 1.1.1' }, { label: 'Tree Item 1.1.2' }, { label: 'Tree Item 1.1.3' }],
-                    },
-                    {
-                      label: 'Tree Item 1.3.2',
-                      children: [{ label: 'Tree Item 1.1.1' }, { label: 'Tree Item 1.1.2' }, { label: 'Tree Item 1.1.3' }],
-                    },
-                    {
-                      label: 'Tree Item 1.3.3',
-                      expanded: true,
-                      children: [
-                        { label: 'Tree Item 1.3.3.1' },
-                        { label: 'Tree Item 1.3.3.2' },
-                        { label: 'Tree Item 1.3.3.3' },
-                      ],
-                    },
+                    { label: 'Tree Item 1.3.3.1' },
+                    { label: 'Tree Item 1.3.3.2' },
+                    { label: 'Tree Item 1.3.3.3' },
                   ],
                 },
-                {
-                  label: 'Tree Item 1.4',
-                  children: [{ label: 'Tree Item 1.4.1' }, { label: 'Tree Item 1.4.2' }, { label: 'Tree Item 1.4.3' }],
-                },
-                { label: 'Tree Item 1.5' },
               ],
             },
             {
-              label: 'Tree Item 2',
-              children: [{ label: 'Tree Item 2.1' }, { label: 'Tree Item 2.2' }, { label: 'Tree Item 2.3' }],
+              label: 'Tree Item 1.4',
+              children: [{ label: 'Tree Item 1.4.1' }, { label: 'Tree Item 1.4.2' }, { label: 'Tree Item 1.4.3' }],
             },
-            {
-              label: 'Tree Item 3',
-            },
-          ];
-
-          window.addEventListener('tree-updated', (e) => {
-            treeData.data = [...e.detail.data];
-          });
-
-          return html`
-        <style>
-          .container {
-            padding: 1rem 10%;
-            display: flex;
-            justify-content: center;
-          }
-
-          rux-tree {
-            width: 18rem;
-            margin: 1rem;
-          }
-        </style>
-        <div class="container">
-          <rux-tree .data="${treeData}"></rux-tree>
-        </div>
-      `;
+            { label: 'Tree Item 1.5' },
+          ],
         },
         {
-          notes: {
-            markdown: Readme,
-          },
+          label: 'Tree Item 2',
+          children: [{ label: 'Tree Item 2.1' }, { label: 'Tree Item 2.2' }, { label: 'Tree Item 2.3' }],
+        },
+        {
+          label: 'Tree Item 3',
+        },
+      ];
+
+      return html`
+      <style>
+        .container {
+          padding: 1rem 10%;
+          display: flex;
+          justify-content: center;
         }
-    )
+
+        rux-tree {
+          width: 18rem;
+          margin: 1rem;
+        }
+      </style>
+      <div class="container">
+        <rux-tree .data="${treeData}"></rux-tree>
+      </div>
+    `;
+    })
     .add(
         'Tree (with Status)',
         () => {
-          window.addEventListener('tree-updated', (e) => {
-            treeData.data = [...e.detail.data];
-          });
-
           const treeData = [
             {
               label: 'Tree Item 1',
@@ -128,7 +113,6 @@ storiesOf('Components|Tree', module)
               children: [{ label: 'Tree Item 4.1', status: 'caution' }, { label: 'Tree Item 4.2', status: 'normal' }],
             },
           ];
-
           return html`
         <style>
           .container {
