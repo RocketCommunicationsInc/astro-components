64e9fadfb24474938cfea4fa1436f0dbb83aaaef
im-cr <gvestudios@gmail.com>
2020-07-02 10:41:38 -0400

    updat to index.js

--- static/index.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-const logoDark = require('./img/astro-logo-small-dark.svg');
-const logoLight = require('./img/astro-logo-small-light.svg');
-
-
-module.exports = {
-	logoDark,
-	logoLight
-};
