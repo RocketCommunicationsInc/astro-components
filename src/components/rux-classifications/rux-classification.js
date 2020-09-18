1e001cb284c114c8db6b5f52f81888529e8b3739
im-cr <gvestudios@gmail.com>
2020-09-18 18:27:26 -0400

    update to component size and marker attributes

--- src/components/rux-classifications/rux-classification.js
+++ src/components/rux-classifications/rux-classification.js
@@ -1,111 +1,157 @@
 import { LitElement, html } from 'lit-element';
-
-
 export class RuxClassification extends LitElement {
   static get properties() {
     return {
+      classification: {
+        type: String
+      },
+      type: {
+        type: String
+      },
       message: {
-	    type: String,
-			},
-			classification: {
-				type: String,
-			},
-			type: {
-				type: String,
-			},
-			visible: {
-				type: Boolean,
-				reflect: true,
-			},
-		};
+        type: String
+      }
+    };
+  }
+  
+  _setClassificationText() {
+    const markClass = this.classification.toLowerCase();
+    const markType = this.type.toLowerCase();
+    let classLabel;
+    
+    // const classType = this.classification;
+    if(markType !== ''){
+      if(markType === 'banner') {
+        switch(markClass) {
+          case 'unclassified':
+            classLabel = 'unclassified';
+            break;
+          case 'controlled':
+            classLabel = 'cui';
+            break;
+          case 'confidential':
+            classLabel = 'confidential';
+            break;
+          case 'secret':
+            classLabel = 'secret';
+            break;
+          case 'top secret':
+            classLabel = 'top secret';
+            break;
+          default:
+            classLabel = 'Top Secret//SCI';
+        }
+
+      } else if (markType === 'tag') {
+        
+        switch(markClass) {
+          case 'unclassified':
+            classLabel = 'u';
+            break;
+          case 'controlled':
+            classLabel = 'cui';
+            break;
+          case 'confidential':
+            classLabel = 'c';
+            break;
+          case 'secret':
+            classLabel = 's';
+            break;
+          case 'top secret':
+            classLabel = 'ts';
+            break;
+          default:
+            classLabel = 'TS//SCI';
+        }
+      }
+    } else {
+      classLabel = 'Please select a marker type';
+    }
+
+    return classLabel;
   }
 
   constructor() {
     super();
     this.message = '';
-    this.classification = 'unclassified';
+    this.classification = '';
     this.type = 'banner';
-    this.visible = false;
+    this.getText = this._setClassificationText;
   }
 
-  render(){
-	  return html`
-	  <style>
-		:host {
-			z-index:1000;
-			display: flex;
-			justify-content: center;
-			align-items: center;
-			align-content: center; 
-			height: auto;
-			padding: 5px 0;
-			
-			box-sizing: border-box;
-			font-size: var(--fontSize);
-			font-weight:bold;
-			font-family: var( --fontFamily);
-			text-transform:uppercase;
-			color: var(--colorBlack, rgb(0, 0, 0));
-			transition: top 0.5s ease;
-		}
+  render() {
+    return html`
+    <style>
+    :host {
+      z-index:1000;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      align-content: center; 
+      height: 26px;
+      padding: 5px 0; 
+      box-sizing: border-box;
+      font-size: var(--fontSize);
+      font-weight:bold;
+      font-family: var( --fontFamily);
+      text-transform:uppercase;
+      color: var(--colorBlack, rgb(0, 0, 0));
+      transition: top 0.5s ease;
+    }
 
-		:host([type='banner']){
-			position: absolute;
-			left: 0;
-			flex-wrap: nowrap;
-			flex-grow: 1;		
-			width: 100%;
-		}
-		
-		:host([type='tag']){
-			position: relative;
-			align-items:baseline;			
-			left: auto;
-			width: auto;
-			height: auto;
-			padding: 4px 15px;
-			border-radius:5px;
-			font-size: var(--fontSizeMD);			
-		}
-		
-		:host([visible]) {
-			top:0;
-		}
+    :host([type='banner']){
+      position: absolute;
+      top: 0;
+      left: 0;
+      flex-wrap: nowrap;
+      flex-grow: 1;		
+      width: 100%;
+    }
+    
+    :host([type='tag']){
+      position: relative;
+      align-items:baseline;			
+      left: auto;
+      width: fit-content;
+      padding: 4px 15px;
+      border-radius:5px;
+      font-size: var(--fontSizeMD);			
+    }
 
-		:host,
-		:host([classification='sci']) {
-			background-color: var(--classificationTopSecretSCIBackgroundColor);
-			color: var(--colorBlack);
-		}
+    :host,
+    :host([classification='topsecret//sci']) {
+      background-color: var(--classificationTopSecretSCIBackgroundColor);
+    }
 
-		:host([classification='topsecret']) {
-			background-color: var(--classificationTopSecretBackgroundColor);
-		}
+    :host([classification='topsecret']),
+    :host([classification='top secret']){
+      background-color: var(--classificationTopSecretBackgroundColor);
+    }
 
-		:host([classification='secret']) {
-			background-color: var(--classificationSecretBackgroundColor);
-			color: var(--colorWhite);
-		}
+    :host([classification='secret']) {
+      background-color: var(--classificationSecretBackgroundColor);
+      color: var(--colorWhite);
+    }
 
-		:host([classification='confidential']) {
-		  background-color: var(--classificationConfidentialBackgroundColor);
-		  color: var(--colorWhite);
-		}
+    :host([classification='confidential']) {
+      background-color: var(--classificationConfidentialBackgroundColor);
+      color: var(--colorWhite);
+    }
 
-		:host([classification='controlled']) {
-		  background-color: var(--classificationControlledBackgroundColor);
-		  color: var(--colorWhite);
-		}
+    :host([classification='controlled']) {
+      background-color: var(--classificationControlledBackgroundColor);
+      color: var(--colorWhite);
+    }
 
-		:host([classification='unclassified']) {
-		  background-color: var(--classificationUnclassifiedBackgroundColor);
-		  color: var(--colorWhite);
-		}
-	  </style>
-	  
-	  <div class="rux-classification__message">${this.message}</div>
-	 
-	  `;
+    :host([classification='unclassified']) {
+      background-color: var(--classificationUnclassifiedBackgroundColor);
+      color: var(--colorWhite);
+    }
+    </style>
+    
+    <div class="rux-classification__message">${this.getText()}${this.message}</div>
+
+    `;
   }
 }
 customElements.define('rux-classification-marking', RuxClassification);
--- stories/rux-classifications.stories.js
+++ stories/rux-classifications.stories.js
@@ -1,5 +1,5 @@
 import { html, render } from 'lit-html';
-import { text, select, withKnobs } from '@storybook/addon-knobs';
+import { text, select, array, withKnobs } from '@storybook/addon-knobs';
 
 
 
@@ -9,101 +9,92 @@ import Readme from '../src/components/rux-classifications/README.md';
 
 export default {
   title: 'Components|Classification',
-  decorators: [withKnobs, withKnobs]
+  decorators: [withKnobs]
 };
 
 
 export const ClassificationBanner = () => {
-	const classificationMark = ['sci', 'topsecret', 'secret', 'confidential','controlled','unclassified'];
-	const classification = select('classification', classificationMark, 'secret');
-	const classOptions = ['banner', 'tag'];
-	const classType = select('Type', classOptions, 'banner');
+  const classificationOptions = ['Top Secret//SCI', 'Top Secret', 'Secret', 'Confidential','Controlled','Unclassified'];
+  const classification = array('Classification', classificationOptions[2].toLowerCase());
   
-	const messageText = text(
-		'Classification Message',
-		`Top Secret//Sci`,
-	);
+  const markerOptions = ['banner', 'tag'];
+  const markerType = select('Banner Type', markerOptions, 'Banner');
+  
+  const markerText = text('Marker Text','');
 
-	return html`
-	  <div style="display: flex; flex-flow: column; justify-content: center;">
-			<rux-classification-marking 
-				?visible="${true}"
-				type="${classType}"
-				classification="${classification}"
-				message="${messageText}">
-			</rux-classification-marking>
-	  </div>
-	`;
-  };
-  
-  ClassificationBanner.story = {
-	parameters: {
-	  exports: {
-		render,
-		html,
-	  },
-	  readme: {
-		sidebar: Readme,
-	  },
-	},
+  return html`
+    <div style="display: flex; flex-flow: column; justify-content: center;">
+      <rux-classification-marking
+        type="${markerType}"
+        classification="${classification}"
+        message="${markerText}">
+      </rux-classification-marking>
+    </div>
+  `;
+};
+
+ClassificationBanner.story = {
+  parameters: {
+    exports: {
+      render,
+      html,
+    },
+    readme: {
+      sidebar: Readme,
+    },
+  },
 };
 
 export const AllClassificationBanners = () => html`
     <div style="display: flex; flex-flow: column; justify-content: center; margin:20px;">
       <div style="display: flex; position: relative; height: 40px; margin-bottom: 20px; overflow: hidden;">
         <rux-classification-marking
-		  		visible
-		  		type="banner"
-          classification="sci"
-          message="Top Secret//SCI"
+          type="banner"
+          classification="top secret//sci"
+          message=""
         ></rux-classification-marking>
       </div>
-			
-			<div style="display: flex; position: relative; height: 40px; margin-bottom: 20px; overflow: hidden;">
-				<rux-classification-marking
-					visible
-					type="banner"
-					classification="topsecret"
-					message="Top Secret"
-				></rux-classification-marking>
-			</div>
-			
-			<div style="display: flex; position: relative; height: 40px; margin-bottom: 20px; overflow: hidden;">
-				<rux-classification-marking
-					visible
-					type="banner"
-					classification="secret"
-					message="Secret"
-				></rux-classification-marking>
-			</div>
-		
-			<div style="display: flex; position: relative; height: 40px; margin-bottom: 20px; overflow: hidden;">
-				<rux-classification-marking
-					visible
-					type="banner"
-					classification="confidential"
-					message="Confidential"
-				></rux-classification-marking>
-			</div>
-		
-			<div style="display: flex; position: relative; height: 40px; margin-bottom: 20px; overflow: hidden;">
-				<rux-classification-marking
-					visible
-					type="banner"
-					classification="controlled"
-					message="CUI"
-				></rux-classification-marking>
-			</div>
-	  
-	  	<div style="display: flex; position: relative; height: 40px; margin-bottom: 20px; overflow: hidden;">
+      
+      <div style="display: flex; position: relative; height: 40px; margin-bottom: 20px; overflow: hidden;">
         <rux-classification-marking
-					visible
-					type="banner"
-          classification="unclassified"
-          message="Unclassified"
+          type="banner"
+          classification="top secret"
+          message=""
         ></rux-classification-marking>
-	  	</div>	  
-		</div>
+      </div>
+      
+      <div style="display: flex; position: relative; height: 40px; margin-bottom: 20px; overflow: hidden;">
+        <rux-classification-marking
+          type="banner"
+          classification="secret"
+          message=""
+        ></rux-classification-marking>
+      </div>
+    
+      <div style="display: flex; position: relative; height: 40px; margin-bottom: 20px; overflow: hidden;">
+        <rux-classification-marking
+          type="banner"
+          classification="confidential"
+          message=""
+        ></rux-classification-marking>
+      </div>
+    
+      <div style="display: flex; position: relative; height: 40px; margin-bottom: 20px; overflow: hidden;">
+        <rux-classification-marking
+          type="banner"
+          classification="controlled"
+          message=""
+        ></rux-classification-marking>
+      </div>
+    
+      <div style="display: flex; position: relative; height: 40px; margin-bottom: 20px; overflow: hidden;">
+        <rux-classification-marking
+          type="banner"
+          classification="unclassified"
+          message=""
+        ></rux-classification-marking>
+      </div>	  
+    </div>
 `;
 
 AllClassificationBanners.story = {
@@ -121,62 +112,56 @@ AllClassificationBanners.story = {
 
 export const ClassificationTags = () => html`
     <div style="display: flex; flex-flow: column; justify-content: flex-start; width: 400px; margin:60px auto;">	  
-			
-			<div style="display: flex; align-items:baseline; position: relative; height: 40px; margin-bottom: 20px; overflow: hidden;">
-				<p style="display:flex; width:225px; font-size:14px; font-style:italic; color:#d5d7d9;">Top Secret//SCI</p>
-				<rux-classification-marking
-					visible
-					type="tag"
-					classification="sci"
-					message="TS//SCI"
-				></rux-classification-marking>
-			</div>
+      
+      <div style="display: flex; align-items:baseline; position: relative; height: 40px; margin-bottom: 20px; overflow: hidden;">
+        <p style="display:flex; width:225px; font-size:14px; font-style:italic; color:#d5d7d9;">Top Secret//SCI</p>
+        <rux-classification-marking
+          type="tag"
+          classification="top secret//sci"
+          message=""
+        ></rux-classification-marking>
+      </div>
 
-			<div style="display: flex; align-items:baseline; position: relative; height: 40px; margin-bottom: 20px; overflow: hidden;">
-			<p style="display:flex; width:225px; font-size:14px; font-style:italic; color:#d5d7d9;">Top Secret</p>	
-			<rux-classification-marking
-					visible
-					type="tag"
-					classification="topsecret"
-					message="TS"
-				></rux-classification-marking>
-			</div>
-			<div style="display: flex; align-items:baseline;  position: relative; height: 40px; margin-bottom: 20px; overflow: hidden;">
-			<p style="display:flex; width:225px; font-size:14px; font-style:italic; color:#d5d7d9;">Secret</p>	
-			<rux-classification-marking
-					visible
-					type="tag"
-					classification="secret"
-					message="S"
-				></rux-classification-marking>
-			</div>
-			<div style="display: flex; align-items:baseline; position: relative; height: 40px; margin-bottom: 20px; overflow: hidden;">
-			<p style="display:flex; width:225px; font-size:14px; font-style:italic; color:#d5d7d9;">Confidential</p>	
-			<rux-classification-marking
-					visible
-					type="tag"
-					classification="confidential"
-					message="C"
-				></rux-classification-marking>
-			</div>
-			<div style="display: flex; align-items:baseline; position: relative; height: 40px; margin-bottom: 20px; overflow: hidden;">
-				<p style="display:flex; width:225px; font-size:14px; font-style:italic; color:#d5d7d9;">Controlled Unclassified</p>	
-				<rux-classification-marking
-					visible
-					type="tag"
-					classification="controlled"
-					message="CUI"
-				></rux-classification-marking>
-			</div>
-			<div style="display: flex; align-items:baseline; position: relative; height: 40px; margin-bottom: 20px; overflow: hidden;">
-				<p style="display: flex; width:225px; font-size:13px; font-style:italic; color:#d5d7d9;">Uncontrolled Unclassified</p>	
-				<rux-classification-marking
-					visible
-					type="tag"
-					classification="unclassified"
-					message="U"
-				></rux-classification-marking>
-			</div>
+      <div style="display: flex; align-items:baseline; position: relative; height: 40px; margin-bottom: 20px; overflow: hidden;">
+        <p style="display:flex; width:225px; font-size:14px; font-style:italic; color:#d5d7d9;">Top Secret</p>	
+        <rux-classification-marking
+          type="tag"
+          classification="top secret"
+          message=""
+        ></rux-classification-marking>
+      </div>
+      <div style="display: flex; align-items:baseline;  position: relative; height: 40px; margin-bottom: 20px; overflow: hidden;">
+        <p style="display:flex; width:225px; font-size:14px; font-style:italic; color:#d5d7d9;">Secret</p>	
+        <rux-classification-marking
+          type="tag"
+          classification="secret"
+          message=""
+        ></rux-classification-marking>
+      </div>
+      <div style="display: flex; align-items:baseline; position: relative; height: 40px; margin-bottom: 20px; overflow: hidden;">
+        <p style="display:flex; width:225px; font-size:14px; font-style:italic; color:#d5d7d9;">Confidential</p>	
+        <rux-classification-marking
+          type="tag"
+          classification="confidential"
+          message=""
+        ></rux-classification-marking>
+      </div>
+      <div style="display: flex; align-items:baseline; position: relative; height: 40px; margin-bottom: 20px; overflow: hidden;">
+        <p style="display:flex; width:225px; font-size:14px; font-style:italic; color:#d5d7d9;">Controlled Unclassified</p>	
+        <rux-classification-marking
+          type="tag"
+          classification="controlled"
+          message=""
+        ></rux-classification-marking>
+      </div>
+      <div style="display: flex; align-items:baseline; position: relative; height: 40px; margin-bottom: 20px; overflow: hidden;">
+        <p style="display: flex; width:225px; font-size:13px; font-style:italic; color:#d5d7d9;">Uncontrolled Unclassified</p>	
+        <rux-classification-marking
+          type="tag"
+          classification="unclassified"
+          message=""
+        ></rux-classification-marking>
+      </div>
 
     </div>
   `;
