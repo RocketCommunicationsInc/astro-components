e3cbdff7176d5bcf89b85e1f3fdfc73b95ebb630
im-cr <gvestudios@gmail.com>
2020-12-03 15:30:10 -0500

    astro-926 toggle update

--- src/components/rux-toggle/rux-toggle.js
+++ src/components/rux-toggle/rux-toggle.js
@@ -64,8 +64,8 @@ export class RuxToggle extends LitElement {
 
         content: "";
         border-radius: 5.5px;
-        border: 1px solid var(--toggleTrackBorderColor, rgb(122, 122, 122));
-        background-color: var(--toggleTrackBackgroundColor, rgb(204, 204, 204));
+        border: 1px solid var(--toggleTrackBorderColor);
+        background-color: var(--toggleTrackBackgroundColor);
 
         height: 11px;
         width: 32px;
@@ -75,11 +75,8 @@ export class RuxToggle extends LitElement {
 
       /* Track */
       .rux-toggle__input:checked + .rux-toggle__button::before {
-        background-color: var(
-          --toggleSelectedTrackBackgroundColor,
-          rgb(100, 217, 255)
-        );
-        border-color: var(--toggleSelectedTrackBorderColor, rgb(27, 122, 153));
+        background-color: var(--toggleSelectedTrackBackgroundColor);
+        border-color: var(--toggleSelectedTrackBorderColor);
       }
 
       .rux-toggle__button::after {
@@ -90,36 +87,30 @@ export class RuxToggle extends LitElement {
         width: 19px;
 
         border-radius: 50%;
-        border: 1px solid var(--toggleThumbBorderColor, #fff);
-        background-color: var(--toggleThumbBackgroundColor, rgb(255, 255, 255));
+        border: 1px solid var(--toggleThumbBorderColor);
+        background-color: var(--toggleThumbBackgroundColor);
 
         transition: 0.167s left ease-in-out, 0.167s border-color ease-in-out;
       }
 
       .rux-toggle__input:disabled + .rux-toggle__button::after {
-        background-color: var(
-          --toggleDisabledThumbBackgroundColor,
-          rgb(163, 163, 163)
-        );
-        border-color: var(--toggleDisabledThumbBorderColor, rgb(163, 163, 163));
+        background-color: var(--toggleDisabledThumbBackgroundColor);
+        border-color: var(--toggleDisabledThumbBorderColor);
       }
 
       .rux-toggle__input:checked + .rux-toggle__button::after {
         left: calc(50% + 2px);
-        border-color: var(--toggleSelectedThumbBorderColor, #fff);
+        border-color: var(--toggleSelectedThumbBorderColor);
       }
 
       .rux-toggle__input:checked:disabled + .rux-toggle__button::after {
-        border-color: var(
-          --toggleDisabledSelectedThumbBorderColor,
-          rgb(163, 163, 163)
-        );
-        cursor: not-allowed;
+        border-color: var(--toggleDisabledSelectedThumbBorderColor);
+        cursor: var(--disabledCursor);
       }
 
       .rux-toggle__input:disabled + .rux-toggle__button::before {
-        opacity: var(--disabledOpacity, 0.4);
-        cursor: not-allowed;
+        opacity: var(--disabledOpacity);
+        cursor: var(--disabledCursor);
       }
       </style>      
       <div class='rux-toggle'>
