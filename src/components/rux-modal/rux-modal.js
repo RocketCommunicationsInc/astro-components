502e28f1785dde81c66d7feb21cd5e956f8ba845
im-cr <gvestudios@gmail.com>
2020-12-03 17:10:05 -0500

    update ot theme

--- src/components/rux-modal/rux-modal.js
+++ src/components/rux-modal/rux-modal.js
@@ -171,7 +171,7 @@ export class RuxModal extends LitElement {
           flex-direction: column;
           flex-grow: 1;
           padding: 1rem;
-          color: var(--fontColor, #fff);
+          color: var(--defaultText);
         }
 
         rux-icon {
