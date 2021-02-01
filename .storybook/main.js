15e7bba9ec518d4179da900e847384873dfbcb6c
Aleksandr Antonov <antonov.a.aleksandr@gmail.com>
2021-02-01 12:39:30 -0700

    add native shims into main.js

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,12 +1,15 @@
+import "@webcomponents/custom-elements/custom-elements.min";
+import "@webcomponents/custom-elements/src/native-shim.js";
+
 module.exports = {
   // stories: ['../stories/**/*.stories.js'], // load stories via preview.js per: https://github.com/storybookjs/storybook/issues/9793
   addons: [
-    'storybook-readme',
-    'storybook-addon-themes', // preview-only theme picker
-    '@storybook/addon-knobs',
-    '@storybook/addon-a11y',
-    '@storybook/addon-links',
-    '@storybook/addon-actions',
+    "storybook-readme",
+    "storybook-addon-themes", // preview-only theme picker
+    "@storybook/addon-knobs",
+    "@storybook/addon-a11y",
+    "@storybook/addon-links",
+    "@storybook/addon-actions",
     // '@storybook/addon-viewport', bug present, enabled when fixed: https://github.com/storybookjs/storybook/issues/10204
-  ]
+  ],
 };
--- src/astro-app.js
+++ src/astro-app.js
@@ -1,4 +1,3 @@
-import "@webcomponents/custom-elements/custom-elements.min";
 import AstroApp from "./AstroApp.js";
 
 window.customElements.define("astro-app", AstroApp);
