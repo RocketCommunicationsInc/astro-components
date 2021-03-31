9b2d49b513c3552906c8d6d5bce2b11e829e9673
Mark Anthony Cianfrani <markacianf@gmail.com>
2021-03-31 12:02:47 -0400

    fix: aos time not showing

--- stories/rux-clock.stories.js
+++ stories/rux-clock.stories.js
@@ -56,11 +56,6 @@ Clock.story = {
 };
 
 export const ClockWithAosLos = () => {
-  function dateWrapper(name, defaultValue) {
-    const stringTimestamp = date(name, defaultValue);
-    return new Date(stringTimestamp);
-  }
-
   const timezones = {
     Guam: 'Pacific/Guam',
     Hawaii: 'Pacific/Honolulu',
@@ -75,8 +70,8 @@ export const ClockWithAosLos = () => {
   };
 
   const timezoneKnob = select('Timezone', timezones, 'UTC');
-  const aosKnob = dateWrapper('AOS', new Date(1557503698781));
-  const losKnob = dateWrapper('LOS', new Date('2019-05-10T16:21:12.000Z'));
+  const aosKnob = '1557503698781';
+  const losKnob = '2019-05-10T16:21:12.000Z';
   const hideTimezoneKnob = boolean('Hide Timezone', false);
   const hideDateKnob = boolean('Hide DOY', false);
   const smallKnob = boolean('Small Version', false);
@@ -84,8 +79,8 @@ export const ClockWithAosLos = () => {
     <div style="padding: 10%; display: flex; justify-content: center;">
       <rux-clock
         .timezone="${timezoneKnob}"
-        aos="${aosKnob.toString()}"
-        los="${losKnob.toString()}"
+        aos="${aosKnob}"
+        los="${losKnob}"
         ?hideTimezone="${hideTimezoneKnob}"
         ?hideDate="${hideDateKnob}"
         ?small="${smallKnob}"
