26573955e2f8c9f8753d03f1eed83f5ba0adbf77
NicDominguez <nic.dominguez@rocketcom.com>
2021-07-02 14:29:05 -0700

    style: syncs shadow dom class names and styles with css class names and styles

--- src/components/rux-segmented-button/rux-segmented-button.js
+++ src/components/rux-segmented-button/rux-segmented-button.js
@@ -1,197 +1,215 @@
-import { LitElement, html } from 'lit-element';
-import RuxUtils from '../rux-utils/string.js';
+import { LitElement, html } from 'lit-element'
+import RuxUtils from '../rux-utils/string.js'
 
 export class RuxSegmentedButton extends LitElement {
-  static get properties() {
-    return {
-      data: {
-        type: Array,
-      },
-      selected: {
-        reflect: true,
-        type: String,
-      },
-    };
-  }
+    static get properties() {
+        return {
+            data: {
+                type: Array,
+            },
+            selected: {
+                reflect: true,
+                type: String,
+            },
+        }
+    }
 
-  constructor() {
-    super();
-    this.data = [{ label: 'No data passed' }];
-    this._selected = '';
-  }
+    constructor() {
+        super()
+        this.data = [{ label: 'No data passed' }]
+        this._selected = ''
+    }
 
-  get selected() {
-    return this._selected;
-  }
+    get selected() {
+        return this._selected
+    }
 
-  set selected(label) {
-    const prevSelection = this.selected;
-    this._selected = label;
-    this.data.forEach((segment) => {
-      segment.selected = segment.label === label;
-    });
-    this.requestUpdate('selected', prevSelection);
-  }
-
-  deselectSelected() {
-    const previousSelectedElement = this.querySelectorAll('input[checked]');
-    previousSelectedElement.forEach((element) => {
-      element.checked = false;
-    });
-  }
-
-  handleChange(event) {
-    this.selected = event.target.value;
-    this.dispatchEvent(
-        new Event('change', {
-          bubbles: true,
-          composed: true,
+    set selected(label) {
+        const prevSelection = this.selected
+        this._selected = label
+        this.data.forEach((segment) => {
+            segment.selected = segment.label === label
         })
-    );
-  }
+        this.requestUpdate('selected', prevSelection)
+    }
 
-  connectedCallback() {
-    super.connectedCallback();
+    deselectSelected() {
+        const previousSelectedElement = this.querySelectorAll('input[checked]')
+        previousSelectedElement.forEach((element) => {
+            element.checked = false
+        })
+    }
 
-    const initialSelection = this.data.find((segment) => segment.selected) || this.data[0];
-    this.selected = initialSelection.label;
-  }
+    handleChange(event) {
+        this.selected = event.target.value
+        this.dispatchEvent(
+            new Event('change', {
+                bubbles: true,
+                composed: true,
+            })
+        )
+    }
 
+    connectedCallback() {
+        super.connectedCallback()
 
-  _slugify(label) {
-    return `${RuxUtils.stringToSlug(label)}`;
-  }
+        const initialSelection =
+            this.data.find((segment) => segment.selected) || this.data[0]
+        this.selected = initialSelection.label
+    }
 
-  render() {
-    return html`
-      <style>
-        :host {
-          box-sizing: border-box;
-        }
+    _slugify(label) {
+        return `${RuxUtils.stringToSlug(label)}`
+    }
 
-        *,
-        *::before,
-        *::after {
-          box-sizing: inherit;
-        }
+    render() {
+        return html`
+            <style>
+                :host {
+                    box-sizing: border-box;
+                }
 
-        .rux-segmented-buttons {
-          display: inline-flex;
+                *,
+                *::before,
+                *::after {
+                    box-sizing: inherit;
+                }
 
-          height: 1.6875rem;
-          overflow: hidden;
+                .rux-segmented-button {
+                    display: inline-flex;
 
-          padding: 0;
-          margin: 0;
+                    height: 1.6875rem;
+                    overflow: hidden;
 
-          list-style: none;
+                    padding: 0;
+                    margin: 0;
 
-          border-radius: var(--controlBorderRadius);          
-					background-color: var(--segmentedButtonBackgroundColor);
-				}
+                    list-style: none;
 
-        .rux-segmented-button {
-          height: 1.6875rem;
-          width: auto;
-          margin: 0;
-					padding: 0;
-				}		
+                    border-radius: var(--controlBorderRadius);
+                    background-color: var(--segmentedButtonBackgroundColor);
+                }
 
-        .rux-segmented-button label {
-          display: flex;
-          justify-content: center;
-          align-items: center;
+                .rux-segmented-button__segment {
+                    height: 1.6875rem;
+                    width: auto;
+                    margin: 0;
+                    padding: 0;
+                }
 
-          width: auto;
-          height: 1.5625rem;
+                .rux-segmented-button label {
+                    display: flex;
+                    justify-content: center;
+                    align-items: center;
 
-          margin: 0;
-          padding: 0 0.75rem;
+                    width: auto;
+                    height: 1.5625rem;
 
-					border: 1px solid var(--segmentedButtonBorderColor);
-					
+                    margin: 0;
+                    padding: 0 0.75rem;
 
-          color: var(--segmentedButtonTextColor);
+                    border: 1px solid var(--segmentedButtonBorderColor);
 
-          font-size: 1rem;
+                    color: var(--segmentedButtonTextColor);
+                    background-color: var(--segmentedButtonBackgroundColor);
 
-          -webkit-user-select: none;
-          -moz-user-select: none;
-          -ms-user-select: none;
-          user-select: none;
-        }
+                    font-size: 1rem;
 
-        .rux-segmented-button input {
-          display: none !important;
-        }
+                    -webkit-user-select: none;
+                    -moz-user-select: none;
+                    -ms-user-select: none;
+                    user-select: none;
+                }
 
-        .rux-segmented-button label:hover {
-					background-color: var(--segmentedButtonHoverBackgroundColor);
-					border-color: var(--segmentedButtonHoverBorderColor);
-          color: var(--segmentedButtonHoverTextColor);
-          outline: none;
-        }
+                .rux-segmented-button__segment:nth-child(2):not(:last-child)
+                    label {
+                    border-right: none !important;
+                }
+                .rux-segmented-button__segment input {
+                    display: none !important;
+                }
 
-        .rux-segmented-button input:checked + label {
-          background-color: var(--segmentedButtonSelectedBackgroundColor);
-          color: var(--segmentedButtonSelectedTextColor);
-          box-shadow: var(--segmentedButtonSelectedBoxShadow);
-				}
-				
-				.rux-segmented-button input:checked:hover + label{
-					border-color: var(--segmentedButtonSelectedBackgroundColor);
-				}
+                .rux-segmented-button__segment label:hover {
+                    background-color: var(
+                        --segmentedButtonHoverBackgroundColor
+                    );
+                    border-color: var(--segmentedButtonHoverBorderColor);
+                    color: var(--segmentedButtonHoverTextColor);
+                    outline: none;
+                }
 
-				.rux-segmented-button:nth-child(2):not(:last-child) label {
-					border-right: none;
-					border-left: none;
-				}
-				.rux-segmented-button:first-child label{
-					border-radius: var(--controlBorderRadius) 0 0 var(--controlBorderRadius);
+                .rux-segmented-button__segment input:checked + label {
+                    background-color: var(
+                        --segmentedButtonSelectedBackgroundColor
+                    );
+                    color: var(--segmentedButtonSelectedTextColor);
+                    box-shadow: var(--segmentedButtonSelectedBoxShadow);
+                }
 
-				}
-				.rux-segmented-button:last-child label{
-					border-radius: 0 var(--controlBorderRadius) var(--controlBorderRadius) 0;
-				}
+                .rux-segmented-button__segment input:checked:hover + label {
+                    background-color: var(
+                        --segmentedButtonSelectedHoverBackgroundColor
+                    );
+                    color: var(--segmentedButtonSelectedHoverTextColor);
+                    border-color: var(
+                        --segmentedButtonSelectedHoverBorderColor
+                    );
+                }
 
-        /* 
-          OVERRIDE FOR IE 
-          Otherwise all segments get rounded corners. Need to override and re-enable
-          some style definitions.
-        */
+                .rux-segmented-button__segment:nth-child(2):not(:last-child)
+                    label {
+                    border-right: none;
+                    border-left: none;
+                }
+                .rux-segmented-button__segment:first-child label {
+                    border-radius: var(--controlBorderRadius) 0 0
+                        var(--controlBorderRadius);
+                }
+                .rux-segmented-button__segment:last-child label {
+                    border-radius: 0 var(--controlBorderRadius)
+                        var(--controlBorderRadius) 0;
+                }
 
-        .rux-segmented-buttons.style-scope {
-          border-radius: 3px 6px 6px 3px !important;
-        }
-        .rux-segmented-button.style-scope {
-          border-radius: 0;
-          height: auto;
-          border: none;
-          border-right: 1px solid var(--segmentedButtonBorderColor);
-        }
-        /* END OVERRDIDE FOR IE EDGE */
-      </style>
+                /*
+                  OVERRIDE FOR IE
+                  Otherwise all segments get rounded corners. Need to override and re-enable
+                  some style definitions.
+                */
 
-      <ul class="rux-segmented-buttons">
-        ${this.data.map((item) => html`
-            <li class="rux-segmented-button">
-              <input
-                type="radio"
-                name="rux-group"
-                id="${this._slugify(item.label)}"
-                value="${item.label}"
-                ?checked="${item.selected}"
-                data-label="${item.label}"
-                @change=${this.handleChange}
-              />
-              <label for="${this._slugify(item.label)}">
-                ${item.label}
-              </label>
-            </li>
-          `)}
-    </ul>
-    `;
-  }
+                .rux-segmented-button.style-scope {
+                    border-radius: 3px 6px 6px 3px !important;
+                }
+                .rux-segmented-button__segment.style-scope {
+                    border-radius: 0;
+                    height: auto;
+                    border: none;
+                    border-right: 1px solid var(--segmentedButtonBorderColor);
+                }
+                /* END OVERRDIDE FOR IE EDGE */
+            </style>
+
+            <ul class="rux-segmented-button">
+                ${this.data.map(
+                    (item) => html`
+                        <li class="rux-segmented-button__segment">
+                            <input
+                                type="radio"
+                                name="rux-group"
+                                id="${this._slugify(item.label)}"
+                                value="${item.label}"
+                                ?checked="${item.selected}"
+                                data-label="${item.label}"
+                                @change=${this.handleChange}
+                            />
+                            <label for="${this._slugify(item.label)}">
+                                ${item.label}
+                            </label>
+                        </li>
+                    `
+                )}
+            </ul>
+        `
+    }
 }
 
-customElements.define('rux-segmented-button', RuxSegmentedButton);
+customElements.define('rux-segmented-button', RuxSegmentedButton)
